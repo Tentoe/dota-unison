dbdfac94fb0056c6fe5a233fedc3960875966c2e
Keven Kurz PC <keven.kurz@gmail.com>
2017-06-10 20:54:51 +0200

    started integrating redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import App from './App';
+import reducer from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root')); // eslint-disable-line react/jsx-filename-extension
+const store = createStore(reducer);
+
+render(
+  <Provider store={store}><App /></Provider>, // eslint-disable-line react/jsx-filename-extension
+  document.getElementById('root'),
+);
+
+
 registerServiceWorker();
